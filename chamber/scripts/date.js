53998f8d733efb8ba0a5d9f52bc63447e583aa9a
GarrenBYU <mei21004@byui.edu>
2022-10-17 20:14:54 -0600

    Update

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -4,3 +4,13 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 
 dateField.innerHTML = `<em>${fulldate}</em>`;
+
+let weekday = now.getDay();
+let chamberMessage = '';
+console.log(weekday);
+if(weekday === 1 || weekday === 2){
+    chamberMessage = '🤝 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+}
+
+const chamberMeet = document.querySelector('.chamberMeet');
+chamberMeet.innerHTML = `<h2>${chamberMessage}</h2>`;
